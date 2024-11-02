b50e56c471088254753377abb55ba6ee68d0ba45
himel1625 <himelmia1625@gmail.com>
2024-11-02 23:11:32 +0600

    implement tape changing category section

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,38 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 import Card from './Card';
-
 const CoffeeCard = () => {
   const navigate = useNavigate();
   const handelButton = () => {
     navigate('/Coffees');
   };
-  // const [CoffeeData, setCoffeeData] = useState([]);
+  const [CoffeeData, setCoffeeData] = useState([]);
   const data = useLoaderData();
   const { categories } = useParams();
-  console.log(categories);
-  console.log(data);
-
-  // useEffect(() => {
-  //   if (categories) {
-  //     const filterByCoffee = [...data].filter(
-  //       (item) => item.categories === categories,
-  //     );
-  //     setCoffeeData(filterByCoffee);
-  //   } else {
-  //     setCoffeeData(data)
-  //   }
-  // }, [categories, data]);
-
+  useEffect(() => {
+    if (categories) {
+      const filterByCoffee = [...data].filter(
+        (item) => item.category === categories,
+      );
+      setCoffeeData(filterByCoffee);
+    } else {
+      setCoffeeData(data);
+    }
+  }, [categories, data]);
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-12">
-        {data.map((coffee) => (
+        {CoffeeData.map((coffee) => (
           <Card key={coffee.id} coffee={coffee} />
         ))}
       </div>
       <button onClick={handelButton} className="btn btn-warning">
-        view more
+        view All
       </button>
     </div>
   );
